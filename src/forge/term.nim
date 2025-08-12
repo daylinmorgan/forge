804f5d12ff12c6e5a0dd535d43af33bd65d993c5
Daylin Morgan <me@dayl.in>
2025-08-12 14:56:41 +0000

    pre render prefixes

--- src/forge/term.nim
+++ src/forge/term.nim
@@ -2,13 +2,19 @@ import std/strutils
 
 import hwylterm
 
-let prefix = "[bold magenta]forge[/] [yellow]||[/] ".bb
+let prefix = $"[bold magenta]forge[/] [cyan]||[/] ".bb
+let errPrefix = $"[red]error ||[/]".bb
+let warnPrefix = $"[yellow]warn ||[/]".bb
+
+# why did I write these as tempaltes?
+template termWarn(args: varargs[string | BBString, `$`]) =
+  stderr.writeLine prefix, warnPrefix, args.join(" ")
 
 template termEcho*(args: varargs[string | BbString, `$`]) =
-  stderr.writeLine $prefix, args.join(" ")
+  stderr.writeLine prefix, args.join(" ")
 
 template termErr*(args: varargs[string | BbString, `$`]) =
-  stderr.writeLine $prefix, $"[red]error ||[/] ".bb, args.join(" ")
+  stderr.writeLine prefix, errPrefix, args.join(" ")
 
 template termErrQuit*(args: varargs[string | BbString, `$`]) =
   termErr args
